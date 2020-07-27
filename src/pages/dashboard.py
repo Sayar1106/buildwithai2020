cd5284fbd7ec3a365452e7ba0b12c98ee56e84bd
Sayar1106 <sayarbanerjee@gmail.com>
2020-07-27 09:07:09 +0530

    Adding predictions and forecaster visualizations

--- src/pages/dashboard.py
+++ src/pages/dashboard.py
@@ -1,5 +1,23 @@
 import streamlit as st
+from datetime import timedelta, datetime
+from src.utils.fetch_url
+from src.utils.load_data
+import plotly.io as pio
+import plotly.express as px
+from plotly import graph_objs as go
+import plotly
 
 
 def main():
     st.title("Dashboard")
+    pio.templates.default = "plotly_dark"
+    date = datetime.today()
+    DATA_URL = ""
+    df = None
+    while True:
+        try:
+            df = load_data(fetch_url(date))
+        except:
+            date = date - timedelta(days=1)
+            continue
+        break
--- src/pages/forecaster.py
+++ src/pages/forecaster.py
@@ -1,8 +1,40 @@
 import streamlit as st
+import pandas as pd
 from datetime import datetime, timedelta
+import os
+import plotly.express as px
+import plotly.io as pio
 
 def main():
    st.title("Forecaster")
-   start_date = datetime(2020, 7, 27)
-   st.sidebar.selectbox("Start Date", [start_date.date() + timedelta(days=x) for x in range(90)])
-   st.sidebar.selectbox("End Date", [start_date.date() + timedelta(days=x) for x in range(90)])
+   pio.templates.default = "plotly_dark"
+   os.path
+   path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', 'Predictions'))
+   states = [f.split(".")[0] for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+   state = st.sidebar.selectbox("Select the State", ["Overall"]+states)
+   st.subheader(state)
+   df = pd.read_csv(path + "/{}.csv".format(state), index_col=0, skiprows=[1,2,3,4], usecols=[0, 1])
+   df["Confirmed"] = round(df["Confirmed"])
+   if st.checkbox("Show raw data"):
+       st.subheader('Raw data')
+       st.write(df)
+   fig = px.bar(x=df.index,
+                y=df["Confirmed"],
+                color=df.index,
+                text=df["Confirmed"],
+                color_discrete_sequence=px.colors.sequential.Reds)
+   fig.update_xaxes(title="Date")
+   fig.update_yaxes(title="Counts")
+   fig.update_traces(textposition='outside',
+                     texttemplate="%{text: .3s}",
+                     hovertemplate=('<br>Count: %{y:,.2f}'
+                                    '<br> Date: %{x}'))
+
+   fig.update_layout(title="Forecasted values for {}".format(state),
+                     width=900,
+                     showlegend=False)
+   st.plotly_chart(fig)
+   
+
+
